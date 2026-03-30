feac6a3527b4cb336f3a3050b23409216082cbf2
OrhanKaynak <kaynakorhan354@gmail.com>
2026-03-31 00:22:37 +0300

    31-03-26

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var menuBar = document.querySelector("#menu_bar");
 var navLinks = document.querySelector(".nav_links");
 
-navLinks.style.top = function(){
-    if(navLinks.style.top == "-450px"){
-        navLinks.style.top = "50px";
+navLinks.style.top = "-450px";
+
+menuBar.addEventListener("click", function(){
+    if(navLinks.style.top === "-450px"){
+        navLinks.style.top = "60px";
     }
     else{
         navLinks.style.top = "-450px";
     }
-}
+});
